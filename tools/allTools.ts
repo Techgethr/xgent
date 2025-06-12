d5366e2d8fa81b7f2e059c12a086e92c5bb5c1f4
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2025-06-12 10:22:14 -0400

    New tools

---
+++ tools/allTools.ts
@@ -0,0 +1,99 @@
+import { getWalletAddressTool } from "./getWalletAddress";
+import { getBalanceTool } from "./getBalance";
+import { getTokenBalancesTool } from "./getTokenBalances";
+import { sendTransactionTool } from "./sendTransaction";
+import { createCheckTool } from "./createCheck";
+import { getChecksTool } from "./getChecks";
+import { cancelCheckTool } from "./cancelCheck";
+import { getTransactionsTool } from "./getTransactions";
+import { createOfferTool } from "./createOffer";
+import { getOffersTool } from "./getOffers";
+import { cancelOfferTool } from "./cancelOffer";
+import { createAMMTool } from "./createAMM";
+import { getAMMsTool } from "./getAMMs";
+import { cashCheckTool } from "./cashCheck";
+
+export interface ToolConfig<T = any> {
+  /**
+   * The definition of the tool.
+   */
+  definition: {
+    type: "function";
+    function: {
+      name: string;
+      description: string;
+      parameters: {
+        type: "object";
+        properties: Record<string, unknown>;
+        required: string[];
+      };
+    };
+  };
+
+  /**
+   * The handler function that will be called when the tool is executed.
+   */
+  handler: (args: T) => Promise<any>;
+}
+
+export const tools: Record<string, ToolConfig> = {
+  // == READ == \\
+  /**
+   * Get the connected wallet address.
+   */
+  get_wallet_address: getWalletAddressTool,
+  /**
+   * Get the balance of a wallet address.
+   */
+   get_balance: getBalanceTool,
+  /**
+   * Get the token balances of a wallet address.
+   */
+   get_token_balances: getTokenBalancesTool,
+  /**
+   * Get the checks of a wallet address.
+   */
+   get_checks: getChecksTool,
+  /**
+   * Get the last transactions of a wallet address.
+   */
+   get_transactions: getTransactionsTool,
+  /**
+   * Get the offers of a wallet address.
+   */
+   get_offers: getOffersTool,
+  /**
+   * Get the AMMs of a wallet address.
+   */
+   get_amms: getAMMsTool,
+
+
+  /**
+   * Send a transaction to another address 
+   */
+   send_transaction: sendTransactionTool,
+  /**
+   * Create and send a new check
+   */
+   send_check: createCheckTool,
+  /**
+   * Cancel a check
+   */
+   cancel_check: cancelCheckTool,
+  /**
+   * Create an offer
+   */
+   create_offer: createOfferTool,
+  /**
+   * Cancel an offer
+   */
+   cancel_offer: cancelOfferTool,
+  /**
+   * Create an AMM
+   */
+   create_amm: createAMMTool,
+  /**
+   * Cash a check
+   */
+   cash_check: cashCheckTool,
+};
